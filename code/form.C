3fbdec56def85c5966986d857d9d21355cee6a1a
Tianbo Liu <tl190@grads-dd.internal.phy.duke.edu>
2017-04-25 12:10:41 -0400

    update

--- code/form.C
+++ code/form.C
@@ -1,3 +1,4 @@
+#include <omp.h>
 #include "Lcore.h"
 
 int main(const int argc, const char * argv[]){
@@ -9,19 +10,19 @@ int main(const int argc, const char * argv[]){
   MODEL::SetMODEL();
   GOLD::SetGOLD();
 
+  Long64_t Nsim = 100000000;
+
   TLorentzVector ki[2], kf[2];
   ki[0].SetXYZM(0, 0, pz, MJpsi);
   
-  Long64_t Nsim = 10000000;
-
   TH1D * h0 = new TH1D("h0", "", 1, 0.0, 2.0);
 
   double weight;
 
   for (Long64_t i = 0; i < Nsim; i++){
-
     weight = GENERATE::BoundStateFormationGold(ki, kf);
     if (weight > 0){
+      //cout << i << endl;
       h0->Fill(1.0, weight);
     }
   }
